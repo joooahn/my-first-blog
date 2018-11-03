da1ed66be3173721bd5dca92c58a324ef3edeb43
joooahn <joooahn@naver.com>
2018-11-03 02:57:31 -0700

    changed

--- blog/models.py
+++ blog/models.py
@@ -6,8 +6,8 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE);
     title = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    due_date = models.DateTimeField(default=timezone.now)
+    priority = models.IntegerField(default=0)
 
     def publish(self):
         self.published_date = timezone.now()
--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import redirect
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.order_by('priority')
     return render(request, 'blog/post_list.html', {'posts' : posts})
 
 def post_detail(request, pk):
@@ -20,9 +20,11 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            post.due_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            #return redirect('post_detail', pk=post.pk)
+            posts = Post.objects.order_by('priority')
+            return render(request, 'blog/post_list.html', {'posts' : posts})
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form' : form})
